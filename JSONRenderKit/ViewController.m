79801df35c377fb99b66716e95ff9437cac68657
陈雄 <478815108@qq.com>
2017-08-02 10:47:05 +0800

    normalization

--- JSONRenderKit/ViewController.m
+++ JSONRenderKit/ViewController.m
@@ -8,25 +8,32 @@
 
 #import "ViewController.h"
 #import "SSBaseRenderController.h"
+#import "CXMacros.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor=[UIColor whiteColor];
+    self.title=@"JSON示例";
+    self.view.backgroundColor                            = [UIColor whiteColor];
+    self.navigationController.navigationBar.translucent  = NO;
+    self.navigationController.navigationBar.barTintColor = kColor(69, 200, 220);
+    self.navigationController.navigationBar.tintColor    = [UIColor whiteColor];
+    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
+    [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
+    self.tableView.tableFooterView=[UIView new];
 }
 
 
-- (IBAction)didPressedBtn:(UIButton *)sender
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    SSBaseRenderController *obj = [[SSBaseRenderController alloc] init];
-    obj.url=@"http://127.0.0.1:5000/appjson";
-    [self.navigationController pushViewController:obj animated:YES];
+    if (indexPath.row==0) {
+        SSBaseRenderController *obj = [[SSBaseRenderController alloc] init];
+        obj.url=@"http://127.0.0.1:5000/appjson";
+        [self.navigationController pushViewController:obj animated:YES];
+    }
 }
-
-
 @end
